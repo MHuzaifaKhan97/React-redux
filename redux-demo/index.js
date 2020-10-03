ec7935aa2ee2e409f3242ac99bb1f944a9118bcb
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2020-10-04 04:49:05 +0500

    Simple Cake Buying Example

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,1 +1,38 @@
-console.log("Hello there")
+const redux = require('redux');
+
+// Initial State
+const initialState = {
+    noOfCakes:10
+}
+
+const BUY_CAKE = "BUY_CAKE";
+
+// Action Creation
+function buyCake(){
+    return {
+        type:BUY_CAKE,
+        info:"First Redux Action",
+    } 
+}
+
+const cakeReducer = (state = initialState,action) => {
+    switch(action.type){
+        case BUY_CAKE : return {
+            ...state, // if more than one state, and we just changed noOfCakes
+            noOfCakes:state.noOfCakes - 1
+        }
+        default: return state;
+    }
+}
+
+// Create Store
+const store = redux.createStore(cakeReducer)
+console.log('Initial State: ',store.getState());
+
+const unsubscribe = store.subscribe(() => console.log("Updated State: ",store.getState()))
+
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+
+unsubscribe()
