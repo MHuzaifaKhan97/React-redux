59a275812059b435d1d8e35efef675ee69f478ec
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2020-10-06 13:53:12 +0500

    Combine multiple reducers using combineReducer()

---
+++ multiple-reducer/index.js
@@ -0,0 +1,64 @@
+const redux = require('redux');
+
+const initialStateCake = {
+    noOfCake: 10,
+}
+
+const initialStateIceCream = {
+    noOfIcecreams: 30
+}
+
+
+const BUY_CAKE = "BUY_CAKE";
+const BUY_ICECREAM = "BUY_ICECREAM"
+
+function buyCake() {
+    return {
+        type: BUY_CAKE,
+        info: 'Cake Buy Successfully'
+    }
+}
+
+function buyIcecream() {
+    return {
+        type: BUY_ICECREAM,
+        info: 'Iceream Buy Successfully'
+    }
+}
+const cakeReducer = (state = initialStateCake, action) => {
+    switch (action.type) {
+        case BUY_CAKE: return {
+            ...state,
+            noOfCake: state.noOfCake - 1
+        }
+        default: return state;
+    }
+}
+const icecreamReducer = (state = initialStateIceCream, action) => {
+    switch (action.type) {
+        case BUY_ICECREAM: return {
+            ...state,
+            noOfIcecreams: state.noOfIcecreams - 1
+        }
+        default: return state;
+    }
+}
+
+const rootReducer = redux.combineReducers({
+    cake:cakeReducer,
+    iceCream:icecreamReducer
+})
+const store = redux.createStore(rootReducer);
+
+console.log("Initil State: ", store.getState())
+
+const unsubscribe = store.subscribe(() => console.log("Updated State: ", store.getState()))
+
+store.dispatch(buyIcecream())
+store.dispatch(buyIcecream())
+store.dispatch(buyIcecream())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+
+unsubscribe()
