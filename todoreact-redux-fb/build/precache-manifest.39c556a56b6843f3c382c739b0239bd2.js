89e037e24f54d0f3184e1b9b94f5d1c4663ca949
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2020-10-07 05:10:53 +0500

    TodoApp using React, Redux & Firebase DB

---
+++ todoreact-redux-fb/build/precache-manifest.39c556a56b6843f3c382c739b0239bd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4423108e77dccf8789fd7cf0833bcbb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "979c8b65e60c87f8cf01",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "b71885b1b316fd175428",
+    "url": "/static/css/main.ec1232a2.chunk.css"
+  },
+  {
+    "revision": "979c8b65e60c87f8cf01",
+    "url": "/static/js/2.52d1e0b1.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/static/js/2.52d1e0b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b71885b1b316fd175428",
+    "url": "/static/js/main.a76892dd.chunk.js"
+  },
+  {
+    "revision": "8bdf347164ca2bd3a3db",
+    "url": "/static/js/runtime-main.f0fadfbe.js"
+  }
+]);
